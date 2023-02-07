3667442d6e656472fef3d9ad6ed5a62677018a31
hlp-pls <53251969+hlp-pls@users.noreply.github.com>
2023-02-07 17:51:28 +0900

    delete target

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.PUBLIC_SETTINGS = {'app_name': 'Fragma', 'author': 'chgnhyk', 'version': '0.0.0', 'environment': 'production'}
